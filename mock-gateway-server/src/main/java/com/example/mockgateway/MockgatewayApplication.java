b0ca68178c39c3a0e19b205358de7c7f10b60093
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-03-14 10:27:43 +0000

    Add mock-gateway-server project (external API simulator)

---
+++ mock-gateway-server/src/main/java/com/example/mockgateway/MockgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockgatewayApplication.class, args);
+	}
+
+}
---
+++ mock-gateway-server/src/test/java/com/example/mockgateway/MockgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
